f53238452a875c21ad95e3d89255f6ccb88ddc7e
Abhi Kumar <abhinavbkumar@protonmail.com>
2025-10-21 15:56:32 -0500

    Added context hook and context component for cart

--- frontend_masters/citr-v9-project-main/padre-ginos/src/App.jsx
+++ frontend_masters/citr-v9-project-main/padre-ginos/src/App.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOftheDay from "./PizzaOfTheDay";
 import Order from "./Order";
+import Header from "./Header";
+import { CartContext } from "./contexts";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padre Gino's - Order Now</h1>
-      <Order />
-      <PizzaOftheDay />
-    </div>
+    <CartContext value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOftheDay />
+      </div>
+    </CartContext>
   );
 };
 
