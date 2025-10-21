b3f848b15b6dfd1e012f3a1e1d306e50e0c3ef55
Abhi Kumar <abhinavbkumar@protonmail.com>
2025-10-21 10:55:28 -0500

    added Order component, useState hook

--- frontend_masters/citr-v9-project-main/padre-ginos/src/App.jsx
+++ frontend_masters/citr-v9-project-main/padre-ginos/src/App.jsx
@@ -1,14 +1,11 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order"
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"} />
-      <Pizza name="Hawaiian" description="why pineapple" image={"/public/pizzas/hawaiian.webp"} />
-      <Pizza name="Americano" description="test" image={"/public/pizzas/big_meat.webp"} />
+      <Order />
     </div>
   )
 };
@@ -16,4 +13,4 @@ const App = () => {
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
